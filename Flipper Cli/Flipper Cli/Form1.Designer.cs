bb1350363d016caa1b86ed5503d4883a11b25e79
GameLord2011 <119822417+gameking2011@users.noreply.github.com>
2024-09-26 13:54:16 -0400

    Add files via upload

---
+++ Flipper Cli/Flipper Cli/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Flipper_Cli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Send = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Recive = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openButon = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.Send.SuspendLayout();
+            this.Recive.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1900",
+            "19200",
+            "38400",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(309, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(306, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Baud Rate";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(436, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(352, 21);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // Send
+            // 
+            this.Send.Controls.Add(this.button1);
+            this.Send.Controls.Add(this.textBox2);
+            this.Send.Location = new System.Drawing.Point(15, 82);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(288, 94);
+            this.Send.TabIndex = 5;
+            this.Send.TabStop = false;
+            this.Send.Text = "Send";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(6, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(6, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(276, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Recive
+            // 
+            this.Recive.Controls.Add(this.textBox1);
+            this.Recive.Location = new System.Drawing.Point(309, 82);
+            this.Recive.Name = "Recive";
+            this.Recive.Size = new System.Drawing.Size(479, 305);
+            this.Recive.TabIndex = 6;
+            this.Recive.TabStop = false;
+            this.Recive.Text = "Recive";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(473, 279);
+            this.textBox1.TabIndex = 0;
+            // 
+            // openButon
+            // 
+            this.openButon.Location = new System.Drawing.Point(12, 215);
+            this.openButon.Name = "openButon";
+            this.openButon.Size = new System.Drawing.Size(75, 23);
+            this.openButon.TabIndex = 7;
+            this.openButon.Text = "Open Port";
+            this.openButon.UseVisualStyleBackColor = true;
+            this.openButon.Click += new System.EventHandler(this.openButon_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Enabled = false;
+            this.closeButton.Location = new System.Drawing.Point(12, 244);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 8;
+            this.closeButton.Text = "Close Port";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.openButon);
+            this.Controls.Add(this.Recive);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Serial Terminal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Send.ResumeLayout(false);
+            this.Send.PerformLayout();
+            this.Recive.ResumeLayout(false);
+            this.Recive.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox Send;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox Recive;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button openButon;
+        private System.Windows.Forms.Button closeButton;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ Flipper Cli/Flipper Cli/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flipper_Cli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getAvaliblePortNames();
+
+        }
+
+        void getAvaliblePortNames()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+        }
+
+
+        private void Cli_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cliSend_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openButon_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.Text == "" || comboBox2.Text == "")
+                {
+                    Recive.Text = "Please select a serial port and/or baud rate.";
+                }
+                else
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.WriteTimeout = 3000;
+                    serialPort1.ReadTimeout = 3000;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                    serialPort1.Open();
+                    progressBar1.Value = 100;
+                    button1.Enabled = true;
+                    textBox2.Enabled = true;
+                    textBox1.Enabled = true;
+                    openButon.Enabled = false;
+                    closeButton.Enabled = true;
+                    comboBox1.Enabled = false;
+                    comboBox2.Enabled = false;
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                textBox1.Text = "Acess to this port is denied. :-(";
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            Console.Beep();
+            button1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox1.Enabled = false;
+            openButon.Enabled = true;
+            closeButton.Enabled = false;
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+            progressBar1.Value = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.WriteLine(textBox2.Text);
+            textBox2.Text = "";
+            try
+            {
+                string v = serialPort1.ReadLine();
+                textBox1.Text = v;
+            }
+            catch (TimeoutException)
+            {
+                textBox1.Text = "device timed out.";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
